f22448a4e96f73ffc80514ca1016fb4dc1b9e8d8
Mahdi Kashkalani <mahdiksk88@gmail.com>
2024-02-19 17:44:33 +0330

    Identity project init

---
+++ src/Infrastructure/LearningPlatform.Identity/ApplicationIdentityDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace LearningPlatform.Identity;
+internal class ApplicationIdentityDbContext : IdentityDbContext
+{
+
+}
---
+++ src/Infrastructure/LearningPlatform.Identity/IdentityServicesRegistration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LearningPlatform.Identity;
+
+public static class IdentityServicesRegistration
+{
+    public static IServiceCollection RegisterIdentityServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContextPool<ApplicationIdentityDbContext>(options =>
+        {
+            options.UseNpgsql(configuration[""]);
+        });
+        services.AddIdentity<IdentityUser, IdentityRole>()
+            .AddEntityFrameworkStores<ApplicationIdentityDbContext>()
+            .AddDefaultTokenProviders();
+        return services;
+    }
+}
