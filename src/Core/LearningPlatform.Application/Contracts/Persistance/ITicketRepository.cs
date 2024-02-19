ee78d3a18ae40194c96e53fc5d766c833fdd6516
Mahdi Kashkalani <mahdiksk88@gmail.com>
2024-02-19 12:36:19 +0330

    Added contracts and implementations init classess

---
+++ src/Core/LearningPlatform.Application/Contracts/Persistance/ITicketRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LearningPlatform.Domain;
+
+namespace LearningPlatform.Application.Contracts.Persistance;
+internal interface ITicketRepository : IGenericRepository<Tickets>
+{
+}
---
+++ src/Infrastructure/LearningPlatform.Persistance/Repositories/BlogRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LearningPlatform.Domain;
+
+namespace LearningPlatform.Persistance.Repositories;
+internal class BlogRepository(ApplicationDbContext context) : GenericRepository<Blog>(context)
+{
+}
