68a0ac4a07c283e7f623b082239459818440a4cb
Mahdi Kashkalani <mahdiksk88@gmail.com>
2024-07-30 19:29:37 +0330

    Added validator classes

---
+++ src/Core/LearningPlatform.Application/DTO/BlogCommentDTOs/Validators/CreateBlogCommentDtoValidator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+
+namespace LearningPlatform.Application.DTO.BlogCommentDTOs.Validators;
+internal class CreateBlogCommentDtoValidator : AbstractValidator<CreateBlogCommentDTO>
+{
+    public CreateBlogCommentDtoValidator()
+    {
+        
+    }
+}
