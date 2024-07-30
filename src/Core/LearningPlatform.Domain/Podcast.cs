78f8caa395b75a29eb1eff9403471252d177d31a
Mahdi Kashkalani <mahdiksk88@gmail.com>
2024-07-30 19:21:55 +0330

    Added base entity properties

--- src/Core/LearningPlatform.Domain/Podcast.cs
+++ src/Core/LearningPlatform.Domain/Podcast.cs
@@ -3,9 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LearningPlatform.Domain.Common;
 
 namespace LearningPlatform.Domain;
-public class Podcast
+public class Podcast : BaseDomainEntity
 {
 
 }
