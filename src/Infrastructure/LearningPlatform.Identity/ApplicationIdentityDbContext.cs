c9fd211c2d785b77a03e07e3b5ba749c988c91a1
Mahdi Kashkalani <mahdiksk88@gmail.com>
2024-07-31 19:26:00 +0330

    Added constructor for context to prevent exception

--- src/Infrastructure/LearningPlatform.Identity/ApplicationIdentityDbContext.cs
+++ src/Infrastructure/LearningPlatform.Identity/ApplicationIdentityDbContext.cs
@@ -4,9 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace LearningPlatform.Identity;
 internal class ApplicationIdentityDbContext : IdentityDbContext
 {
-
+    public ApplicationIdentityDbContext(DbContextOptions<ApplicationIdentityDbContext> options) : base(options)
+    {
+        
+    }
 }
