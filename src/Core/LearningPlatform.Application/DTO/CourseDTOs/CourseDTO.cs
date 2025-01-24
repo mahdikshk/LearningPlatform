a3d2d46193772a9f981fe1e2b984ecf46aeafa53
Mahdi Kashkalani <mahdiksk88@gmail.com>
2025-01-24 18:39:08 +0330

    Updated Course DTO

--- src/Core/LearningPlatform.Application/DTO/CourseDTOs/CourseDTO.cs
+++ src/Core/LearningPlatform.Application/DTO/CourseDTOs/CourseDTO.cs
@@ -5,6 +5,18 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace LearningPlatform.Application.DTO.CourseDTOs;
-public class CourseDTO
+public class CourseDTO : BaseDTO
 {
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public int Teacher_Id { get; set; }
+    public string TeacherName { get; set; }
+    public bool IsFree { get; set; }
+    public int? OriginalPrice { get; set; }
+    public bool HasDiscount { get; set; }
+    public int? DiscountPercentage { get; set; }
+    public DateTime? DiscountValidUntil { get; set; }
+    public DateTime DateOfCreation { get; set; }
+
+
 }
